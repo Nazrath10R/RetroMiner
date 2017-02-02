4a9f81ae1cf47388af121edc0f3405c5b7d7fc8c
Nazrath10R <nazrath.nawaz@yahoo.de>
2017-02-02 17:56:05 +0000

    calculate total re-analysis time

--- pride_reanalysis.sh
+++ pride_reanalysis.sh
@@ -50,6 +50,35 @@ function check_if_file_exists {
 }
 
 mkdir $OUTPUT_FOLDER/$SAMPLE
+
+############################################################
+
+#### Calculate Total runtime ####
+
+function print_time {
+  END=$(date +%s)
+  DIFF=$(( $END - $1 ))
+  dd=$(echo "$DIFF/86400" | bc)
+  dt2=$(echo "$DIFF-86400*$dd" | bc)
+  dh=$(echo "$dt2/3600" | bc)
+  dt3=$(echo "$dt2-3600*$dh" | bc)
+  dm=$(echo "$dt3/60" | bc)
+  ds=$(echo "$dt3-60*$dm" | bc)
+  if [ $dd -gt 0 ]; then
+    echo " ${dd} days and ${dh} hours."
+  elif [ $dh -gt 0 ]; then
+    echo " ${dh} hours and ${dm} minutes."
+  elif [ $dm -gt 0 ]; then
+    echo " ${dm} minutes and ${ds} seconds."
+  else
+    echo " ${ds} seconds."
+  fi
+}
+
+## Start clock time
+START=$(date +%s)
+
+
 # #######################################################
 # ####					Search GUI					 ####
 # #######################################################
