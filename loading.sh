a4ef3f5ada557c216f0e69da3a9afe14e441c451
Nazrath10R <nazrath.nawaz@yahoo.de>
2017-02-02 17:56:16 +0000

    loading progress bar script

---
+++ loading.sh
@@ -0,0 +1,23 @@
+
+echo -ne '██                      (10%)\r'
+sleep 0.5
+echo -ne '████                    (20%)\r'
+sleep 0.5
+echo -ne '██████                  (30%)\r'
+sleep 0.5
+echo -ne '████████                (40%)\r'
+sleep 0.5
+echo -ne '██████████              (50%)\r'
+sleep 0.5
+echo -ne '████████████            (60%)\r'
+sleep 0.5
+echo -ne '██████████████          (70%)\r'
+sleep 0.5
+echo -ne '████████████████        (80%)\r'
+sleep 0.5
+echo -ne '██████████████████      (90%)\r'
+sleep 0.5
+echo -ne '████████████████████   (100%)\r'
+echo -ne '\n'
+
+
