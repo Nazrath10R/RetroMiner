32ff60aff219b20713262873a0be3db8a377ea94
Nazrath10R <nazrath.nawaz@yahoo.de>
2017-02-07 14:42:52 +0000

    missed section from last commit

--- pride_reanalysis.sh
+++ pride_reanalysis.sh
@@ -142,15 +142,129 @@ ls
 echo
 sleep 8
 
+
+# ## Search PRIDE datasets for project of interest
+# ## Search PRIDE datasets for biomarker of interest
+echo
+echo
+echo "Retrieving Data from PRIDE successful"
+echo
+echo "Starting Re-Analysis tools..."
+sleep 10
+
+############################################################
+
+echo
+echo "Input data file:"
+echo "$INPUT_FILE"
+echo
+echo "Parameter file:"
+echo "PARAMETERS"
+echo
+echo "Sample Name:"
+echo "$SAMPLE"
+echo
+echo "Replicates: $REPLICATE"
+echo
+
+
+############################################################
+
+#### Calculate Total runtime ####
+
+function print_time {
+  END=$(date +%s)
+  DIFF=$(( $END - $1 ))
+  dd=$(echo "$DIFF/86400" | bc)
+  dt2=$(echo "$DIFF-86400*$dd" | bc)
+  dh=$(echo "$dt2/3600" | bc)
+  dt3=$(echo "$dt2-3600*$dh" | bc)
+  dm=$(echo "$dt3/60" | bc)
+  ds=$(echo "$dt3-60*$dm" | bc)
+  if [ $dd -gt 0 ]; then
+    echo " ${dd} days and ${dh} hours."
+  elif [ $dh -gt 0 ]; then
+    echo " ${dh} hours and ${dm} minutes."
+  elif [ $dm -gt 0 ]; then
+    echo " ${dm} minutes and ${ds} seconds."
+  else
+    echo " ${ds} seconds."
+  fi
+}
+
+## Start clock time
+START=$(date +%s)
+
+
+#######################################################
+####					Search GUI				   ####
+#######################################################
+
+
+cd /data/home/bt12048/pride_reanalysis/SearchGUI.5/
+
+if [ "$ANALYSIS" -eq 1 ]; then
+	echo
+	echo "Starting SearchGUI for MS-GF+ only..."
+	echo
+	bash loading.sh
+	java -cp SearchGUI-3.2.5.jar eu.isas.searchgui.cmd.SearchCLI -spectrum_files $INPUT_FILE -output_folder $OUTPUT_FOLDER/$SAMPLE -id_params $PARAMETERS -xtandem 0 -myrimatch 0 -ms_amanda 0 -msgf 1 -omssa 0 -comet 0 -tide 0 -andromeda 0 -threads $THREADS
+fi
+
+if [ "$ANALYSIS" -eq 2 ]; then
+	echo
+	echo "Starting SearchGUI for multiple Search Engines..."
+	echo
+	echo "X! Tandem"
+	echo "MyriMatch"
+	echo "MS Amanda"
+	echo "MS-GF+"
+	echo "OMSSA"
+	echo "Comet"
+	# echo "Andromeda"
+	echo
+	bash loading.sh
+	echo
+	java -cp SearchGUI-3.2.5.jar eu.isas.searchgui.cmd.SearchCLI -spectrum_files $INPUT_FILE -output_folder $OUTPUT_FOLDER -id_params $PARAMETERS -xtandem 1 -myrimatch 1 -ms_amanda 0 -msgf 1 -omssa 1 -comet 1 -tide 0 -andromeda 0 -threads $THREADS
+fi
+
+## check if output was produced
+if [ ! -f /data/home/bt12048/pride_reanalysis/outputs/searchgui_out.zip ]; 
+	then echo "Error: SearchGUI did not finish running properly"
+else
+	echo
+	echo "SearchGUI completed successfully"
+	echo
+	echo "Starting PeptideShaker..."
+	echo
+	bash loading.sh
+	echo
+fi
+
+
+#######################################################
+#### 				Peptide Shaker 				   ####
+#######################################################
+
+cd /data/home/bt12048/pride_reanalysis/PeptideShaker.6/
+
+java -cp PeptideShaker-1.14.6.jar eu.isas.peptideshaker.cmd.PeptideShakerCLI -experiment $EXPERIMENT -sample $SAMPLE -replicate $REPLICATE -identification_files $OUTPUT_FOLDER -spectrum_files $INPUT_FILE -id_params $PARAMETERS -out $OUTPUT_FOLDER/$SAMPLE.cpsx -threads $THREADS
+
+## Print Analysis time
+echo
+echo "Total Run-time for this Re-Analysis was:"
+print_time $START
+echo
+
 ## e-mail notification
 mail -s "Apocrita run completed" nazrath.nawaz@yahoo.de <<< "File: $INPUT_FILE"
 
-#######################################################
+# #######################################################
 
-# ## convert PeptideShaker results to .mzidML file
-# java -cp PeptideShaker-1.14.6.jar eu.isas.peptideshaker.cmd.MzidCLI 
-# -in /data/home/bt12048/pride_reanalysis/outputs/result.cpsx -output_file results 
-# -contact_first_name "Nazrath" -contact_last_name "Nawaz" -contact_email "nazrath.nawaz@yahoo.de" 
-# -contact_address "Fogg Building, London" -organization_name "QMUL" -organization_email "m.n.mohamednawaz@se12.qmul.ac.uk" -organization_address "Mile End Road, London" 
-# -spectrum_files /data/home/bt12048/pride_reanalysis/inputs/
+# # ## convert PeptideShaker results to .mzidML file
+# # java -cp PeptideShaker-1.14.6.jar eu.isas.peptideshaker.cmd.MzidCLI 
+# # -in /data/home/bt12048/pride_reanalysis/outputs/result.cpsx -output_file results 
+# # -contact_first_name "Nazrath" -contact_last_name "Nawaz" -contact_email "nazrath.nawaz@yahoo.de" 
+# # -contact_address "Fogg Building, London" -organization_name "QMUL" -organization_email "m.n.mohamednawaz@se12.qmul.ac.uk" -organization_address "Mile End Road, London" 
+# # -spectrum_files /data/home/bt12048/pride_reanalysis/inputs/
 
