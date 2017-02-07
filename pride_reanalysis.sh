2875992e35435f8f0ac87ee2aadf13615bdc1c70
Nazrath10R <nazrath.nawaz@yahoo.de>
2017-02-07 14:40:50 +0000

    working basic API

--- pride_reanalysis.sh
+++ pride_reanalysis.sh
@@ -36,74 +36,9 @@ THREADS=76
 echo
 echo "Starting Re-Analysis Pipeline..."
 echo
-echo "Fetching Data from PRIDE through RESTful API"
-echo
-echo "Retrieving all Projects containing Protein: $PROTEIN"
-echo
-
-
-## protein Q9UN81
-##
-wget https://www.ebi.ac.uk:443/pride/ws/archive/project/list?query=Q9UN81&order=desc
-
-echo "Identifiying Projects..."
-echo
-echo "Project Accession numbers:"
-echo
-grep -o "PXD[0-9][0-9][0-9][0-9][0-9][0-9]" list
-echo
-grep -o "PXD[0-9][0-9][0-9][0-9][0-9][0-9]" list > list2.txt
-
-
-## Identify Assay for Project 1
-echo
-echo "Retrieving protein identifications for project 1 and protein: Q9UN81"
-echo 
-wget http://www.ebi.ac.uk:80/pride/ws/archive/protein/list/project/PXD003411/protein/Q9UN81
-echo
-
-## Retrieve Array Acession number
-grep -o "assayAccession.:.[0-9]*" Q9UN81 > array_accession.txt
-sed -i -e 's/assayAccession.:.//g' array_accession.txt
-echo
-
-## Identify Array data files
-echo "Project 1 and Array 1"
-echo
-wget http://www.ebi.ac.uk:80/pride/ws/archive/file/list/project/PXD003411
-echo
-
-## Download Data files for Array 60796
-echo "Download Data files for Array 1"
-wget https://www.ebi.ac.uk:443/pride/ws/archive/file/list/assay/60796
-echo
-grep -o "mgf...downloadLink...ftp.*mgf" 60796 > downloadLink.sh
-sed -i -e 's/mgf","downloadLink":"/wget /g' downloadLink.sh
-sh downloadLink.sh
-
-## Search PRIDE datasets for project of interest
-## Search PRIDE datasets for biomarker of interest
-
-echo "Retrieving Data from PRIDE successfull"
-echo
-echo "Starting Re-Analysis tools"
-
-############################################################
-
-echo
-echo "Input data file:"
-echo "$INPUT_FILE"
-echo
-echo "Parameter file:"
-echo "PARAMETERS"
-echo
-echo "Sample Name:"
-echo "$SAMPLE"
-echo
-echo "Replicates: $REPLICATE"
-echo
 echo "Creating Output folder"
 echo
+sleep 3
 
 function check_if_file_exists {
   if [ -s $OUTPUT_FOLDER/$SAMPLE ]; then 
@@ -115,94 +50,97 @@ function check_if_file_exists {
 }
 
 mkdir $OUTPUT_FOLDER/$SAMPLE
-
-############################################################
-
-#### Calculate Total runtime ####
-
-function print_time {
-  END=$(date +%s)
-  DIFF=$(( $END - $1 ))
-  dd=$(echo "$DIFF/86400" | bc)
-  dt2=$(echo "$DIFF-86400*$dd" | bc)
-  dh=$(echo "$dt2/3600" | bc)
-  dt3=$(echo "$dt2-3600*$dh" | bc)
-  dm=$(echo "$dt3/60" | bc)
-  ds=$(echo "$dt3-60*$dm" | bc)
-  if [ $dd -gt 0 ]; then
-    echo " ${dd} days and ${dh} hours."
-  elif [ $dh -gt 0 ]; then
-    echo " ${dh} hours and ${dm} minutes."
-  elif [ $dm -gt 0 ]; then
-    echo " ${dm} minutes and ${ds} seconds."
-  else
-    echo " ${ds} seconds."
-  fi
-}
-
-## Start clock time
-START=$(date +%s)
-
-
-#######################################################
-####					Search GUI				   ####
-#######################################################
-
-
-cd /data/home/bt12048/pride_reanalysis/SearchGUI.5/
-
-if [ "$ANALYSIS" -eq 1 ]; then
-	echo
-	echo "Starting SearchGUI for MS-GF+ only..."
-	echo
-	bash loading.sh
-	java -cp SearchGUI-3.2.5.jar eu.isas.searchgui.cmd.SearchCLI -spectrum_files $INPUT_FILE -output_folder $OUTPUT_FOLDER/$SAMPLE -id_params $PARAMETERS -xtandem 0 -myrimatch 0 -ms_amanda 0 -msgf 1 -omssa 0 -comet 0 -tide 0 -andromeda 0 -threads $THREADS
-fi
-
-if [ "$ANALYSIS" -eq 2 ]; then
-	echo
-	echo "Starting SearchGUI for multiple Search Engines..."
-	echo
-	echo "X! Tandem"
-	echo "MyriMatch"
-	echo "MS Amanda"
-	echo "MS-GF+"
-	echo "OMSSA"
-	echo "Comet"
-	# echo "Andromeda"
-	echo
-	bash loading.sh
-	echo
-	java -cp SearchGUI-3.2.5.jar eu.isas.searchgui.cmd.SearchCLI -spectrum_files $INPUT_FILE -output_folder $OUTPUT_FOLDER -id_params $PARAMETERS -xtandem 1 -myrimatch 1 -ms_amanda 0 -msgf 1 -omssa 1 -comet 1 -tide 0 -andromeda 0 -threads $THREADS
-fi
-
-## check if output was produced
-if [ ! -f /data/home/bt12048/pride_reanalysis/outputs/searchgui_out.zip ]; 
-	then echo "Error: SearchGUI did not finish running properly"
-else
-	echo
-	echo "SearchGUI completed successfully"
-	echo
-	echo "Starting PeptideShaker..."
-	echo
-	bash loading.sh
-	echo
-fi
-
-
-#######################################################
-#### 				Peptide Shaker 				   ####
-#######################################################
-
-cd /data/home/bt12048/pride_reanalysis/PeptideShaker.6/
-
-java -cp PeptideShaker-1.14.6.jar eu.isas.peptideshaker.cmd.PeptideShakerCLI -experiment $EXPERIMENT -sample $SAMPLE -replicate $REPLICATE -identification_files $OUTPUT_FOLDER -spectrum_files $INPUT_FILE -id_params $PARAMETERS -out $OUTPUT_FOLDER/$SAMPLE.cpsx -threads $THREADS
-
-## Print Analysis time
 echo
-echo "Total Run-time for this Re-Analysis was:"
-print_time $START
+echo "Fetching Data from PRIDE through RESTful API"
 echo
+echo "Retrieving all Projects containing Protein: $PROTEIN"
+echo
+sleep 6
+
+## protein Q9UN81
+## Search all Projects containing protein: Q9UN81
+cd $OUTPUT_FOLDER/$SAMPLE
+echo
+echo "Identifiying Projects..."
+echo
+wget https://www.ebi.ac.uk:443/pride/ws/archive/project/list?query=Q9UN81&order=desc
+echo
+echo
+echo
+echo
+echo "Projects containing Q9UN81 identified"
+echo
+echo "Project Accession numbers:"
+echo
+grep -o "PXD[0-9][0-9][0-9][0-9][0-9][0-9]" list?query=Q9UN81
+echo
+grep -o "PXD[0-9][0-9][0-9][0-9][0-9][0-9]" list?query=Q9UN81 > pxd_list.txt
+sleep 8
+
+## Identify Array for Project 1
+echo
+echo "Retrieving protein identification array for project 1 and protein: Q9UN81"
+echo 
+wget http://www.ebi.ac.uk:80/pride/ws/archive/protein/list/project/PXD003411/protein/Q9UN81
+echo
+echo
+echo
+sleep 8
+
+## Retrieve Array Acession number
+echo "Identifying Arrays..."
+echo
+grep -o "assayAccession.:.[0-9]*" Q9UN81 > array_accession.txt
+sed -i -e 's/assayAccession.:.//g' array_accession.txt
+echo
+echo "Arrays identified"
+echo
+echo Array Accession number: "$(cat array_accession.txt)"
+echo
+echo
+sleep 8
+
+## Identify Array data files
+
+# Retrieve all data files for Project 1
+# echo "Project 1 and Array 1"
+# echo
+# wget http://www.ebi.ac.uk:80/pride/ws/archive/file/list/project/PXD003411
+# echo
+
+## Download Data files for Array 60796
+echo
+echo "Starting to Download Data files for Array 1"
+echo
+echo
+wget https://www.ebi.ac.uk:443/pride/ws/archive/file/list/assay/60796 
+echo
+echo
+echo
+grep -o "mgf...downloadLink...ftp.*mgf" 60796 > downloadLink.sh
+sed -i -e 's/mgf","downloadLink":"/wget /g' downloadLink.sh
+sh downloadLink.sh
+echo
+echo
+echo "Download successful"
+echo
+
+## remove unnecessary files
+# echo "removing temp files..."
+# rm Q9UN81
+# # rm PXD003411
+# rm 60796
+# rm list?query=Q9UN81
+# rm array_accession.txt
+# rm downloadLink.sh
+# rm pxd_list.txt
+
+echo
+echo "Files Downloaded:"
+echo
+ls
+echo
+sleep 8
 
 ## e-mail notification
 mail -s "Apocrita run completed" nazrath.nawaz@yahoo.de <<< "File: $INPUT_FILE"
