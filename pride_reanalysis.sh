a56d85a16d62d20841d467bbdec5647f05fcc4a6
Nazrath10R <nazrath.nawaz@yahoo.de>
2017-02-19 12:37:02 +0000

    PeptideShaker on parallelised results (hard-coded)

--- pride_reanalysis.sh
+++ pride_reanalysis.sh
@@ -300,7 +300,20 @@ fi
 
 cd /data/home/bt12048/pride_reanalysis/PeptideShaker.6/
 
-java -cp PeptideShaker-1.14.6.jar eu.isas.peptideshaker.cmd.PeptideShakerCLI -experiment $EXPERIMENT -sample $SAMPLE -replicate $REPLICATE -identification_files $OUTPUT_FOLDER -spectrum_files $INPUT_FILE -id_params $PARAMETERS -out $OUTPUT_FOLDER/$SAMPLE.cpsx -threads $THREADS
+## Run PeptideShaker
+echo
+java -Xmx100G -cp PeptideShaker-1.14.6.jar eu.isas.peptideshaker.cmd.PeptideShakerCLI -experiment $EXPERIMENT -sample $SAMPLE -replicate $REPLICATE -identification_files $OUTPUT_FOLDER -spectrum_files $INPUT_FILE -id_params $PARAMETERS -out $OUTPUT_FOLDER/$SAMPLE.cpsx -threads $THREADS
+
+# java -Xmx100G -cp PeptideShaker-1.14.6.jar eu.isas.peptideshaker.cmd.PeptideShakerCLI -experiment inflammation -sample inflammation_dataset_cyt_con_2a -replicate 1 -identification_files /data/home/bt12048/pride_reanalysis/outputs/searchgui_out -spectrum_files /data/home/bt12048/pride_reanalysis/inputs/HUVEC_cyt_con_2a.mgf -id_params /data/home/bt12048/pride_reanalysis/parameters/inflammation.par -out /data/home/bt12048/pride_reanalysis/outputs/inflammation_dataset_cyt_con_2a.cpsx -threads 18
+
+if [ ! -f $OUTPUT_FOLDER/$SAMPLE.cpsx ]; 
+	then echo "Error: PeptideShaker did not finish running properly"
+else
+	echo
+	echo "PeptideShaker successfully completed"
+	echo
+fi
+
 
 ## Print Analysis time
 echo
