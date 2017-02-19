b5e272650b05d102689b669323c6bc160ea4510f
Nazrath10R <nazrath.nawaz@yahoo.de>
2017-02-19 12:38:15 +0000

    convert PeptideShaker results into mzidML files
    potential use of Ryan’s parser

--- pride_reanalysis.sh
+++ pride_reanalysis.sh
@@ -314,6 +314,17 @@ else
 	echo
 fi
 
+# #######################################################
+
+## convert PeptideShaker results to .mzidML file
+echo "converting PeptideShaker output to mzIdentML files"
+echo
+java -Xmx100G -cp PeptideShaker-1.14.6.jar eu.isas.peptideshaker.cmd.MzidCLI -in /data/home/bt12048/pride_reanalysis/outputs/inflammation_dataset_cyt_con_2a.cpsx -output_file /data/home/bt12048/pride_reanalysis/outputs/inflammation_dataset_cyt_con_2a.mzid -contact_first_name "Nazrath" -contact_last_name "Nawaz" -contact_email "nazrath.nawaz@yahoo.de" -contact_address "Fogg Building, London" -organization_name "QMUL" -organization_email "m.n.mohamednawaz@se12.qmul.ac.uk" -organization_address "Mile End Road, London"
+
+# -spectrum_files /data/home/bt12048/pride_reanalysis/inputs/
+echo
+echo "conversion to mzIdentML successful"
+echo
 
 ## Print Analysis time
 echo
