901a9b3ebb793595f91e09b7b3476f33096daa65
Nazrath10R <nazrath.nawaz@yahoo.de>
2017-02-20 15:37:53 +0000

    Introduced command line variables

--- pride_reanalysis.sh
+++ pride_reanalysis.sh
@@ -4,39 +4,61 @@
 #######    Re-Analysis Pipeline for PRIDE datasets    ########
 ##############################################################
 
-## Human proteome contains fasta file with all three ORF proteins
-# Seperate analysis for projects that contain reshakable files vs
-# non reshakable files
-
+#============================================================#
+# sh variables.sh "Q9UN81" "PXD003406" '/data/home/bt12048/pride_reanalysis/inputs/HUVEC_cyt_con_2a.mgf' '/data/home/bt12048/pride_reanalysis/parameters/inflammation.par' "inflammation" "inflammation_dataset_cyt_con_2a" 2 1 "/data/home/bt12048/pride_reanalysis/outputs/test" 70
+#============================================================#
 
 ######## Variables ########
-PROTEIN="Q9UN81"
-PXD="PXD003406"
-INPUT_FILE='/data/home/bt12048/pride_reanalysis/inputs/HUVEC_cyt_con_2a.mgf'
-PARAMETERS='/data/home/bt12048/pride_reanalysis/parameters/inflammation.par'
-EXPERIMENT="inflammation"
-SAMPLE="inflammation_dataset_cyt_con_2a" 
-ANALYSIS=2
-REPLICATE=1
-OUTPUT_FOLDER="/data/home/bt12048/pride_reanalysis/outputs/test"
-THREADS=70
+
+## Hard-coded
+# PROTEIN="Q9UN81"
+# PXD="PXD003406"
+# INPUT_FILE='/data/home/bt12048/pride_reanalysis/inputs/HUVEC_cyt_con_2a.mgf'
+# PARAMETERS='/data/home/bt12048/pride_reanalysis/parameters/inflammation.par'
+# EXPERIMENT="inflammation"
+# SAMPLE="inflammation_dataset_cyt_con_2a" 
+# ANALYSIS=2
+# REPLICATE=1
+# OUTPUT_FOLDER="/data/home/bt12048/pride_reanalysis/outputs/test"
+# THREADS=70
 
 
-#######################################################
-####                PRIDE Reshake                  ####
-#######################################################
+## Command Line Arguments
+PROTEIN=$1
+PXD=$2
+INPUT_FILE=$3
+PARAMETERS=$4
+EXPERIMENT=$5
+SAMPLE=$6
+ANALYSIS=$7
+REPLICATE=$8
+OUTPUT_FOLDER=$9
+THREADS=${10}
 
-# PRIDE Reshake works for mzIdentML or PRIDE XML files
+echo
+echo
+echo Protein: $PROTEIN
+echo
+echo PXD: $PXD
+echo
+echo INPUT_FILE: $INPUT_FILE
+echo
+echo PARAMETERS: $PARAMETERS
+echo
+echo EXPERIMENT: $EXPERIMENT
+echo
+echo SAMPLE: $SAMPLE
+echo
+echo ANALYSIS: $ANALYSIS
+echo
+echo REPLICATE: $REPLICATE
+echo
+echo OUTPUT_FOLDER: $OUTPUT_FOLDER
+echo
+echo THREADS: $THREADS
+echo
 
-## Pride Reshake
-# requires ssh -X for interactivity
-# java -jar PeptideShaker-1.14.6.jar -pxAccession "PXD003411"
-
-
-#######################################################
-####                    PRIDE API                  ####
-#######################################################
-
+# sh test.sh "Q9UN81" "PXD003406" '/data/home/bt12048/pride_reanalysis/inputs/HUVEC_cyt_con_2a.mgf' '/data/home/bt12048/pride_reanalysis/parameters/inflammation.par' "inflammation" "inflammation_dataset_cyt_con_2a" 2 1 "/data/home/bt12048/pride_reanalysis/outputs/test" 70
 
 
 ############################################################
@@ -66,10 +88,34 @@ function print_time {
 ## Start clock time
 START=$(date +%s)
 
+############################################################
+
+
+#######################################################
+####                    PRIDE API                  ####
+#######################################################
+
+
+
+
+#######################################################
+####                PRIDE Reshake                  ####
+#######################################################
+
+# PRIDE Reshake works for mzIdentML or PRIDE XML files
+
+## Pride Reshake
+# requires ssh -X for interactivity
+# java -jar PeptideShaker-1.14.6.jar -pxAccession "PXD003411"
+
+
 
 #######################################################
 ####                  Search GUI                   ####
 #######################################################
+
+
+
 
 
 #######################################################
@@ -78,15 +124,19 @@ START=$(date +%s)
 
 
 
+
+
 # #######################################################
 # ####              Data filtering                   ####
 # #######################################################
 
 
+
+
+
 # #######################################################
 # ####              Finish Analysis                  ####
 # #######################################################
-
 
 ## Print Analysis time
 echo
@@ -97,13 +147,5 @@ echo
 ## e-mail notification
 mail -s "Apocrita run completed" nazrath.nawaz@yahoo.de <<< "File: $INPUT_FILE"
 
-# #######################################################
-
-
-# ## To Do:
-# # come up with a way to filter results for protein of interest
-# # use ProViewer to vizualise
-# # automate parameters
-# # Final results table
-
+########################################################
 
