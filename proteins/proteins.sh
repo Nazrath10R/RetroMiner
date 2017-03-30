6663c68362c919c06a162e312b532a8e4b461d10
Nazrath10R <nazrath.nawaz@yahoo.de>
2017-03-30 18:48:10 +0100

    UPS2 protein list to be modified for retroelement proteins

---
+++ proteins/proteins.sh
@@ -0,0 +1,156 @@
+
+FILE="myrimatch_Default_Protein_Report.txt"
+
+echo
+echo
+grep "Carbonic anhydrase 1" $FILE
+echo
+echo
+grep "Carbonic anhydrase 2" $FILE
+echo
+echo
+grep "Complement C5" $FILE
+echo
+echo
+grep "Hemoglobin subunit alpha" $FILE
+echo
+echo
+grep "Hemoglobin subunit beta" $FILE
+echo
+echo
+grep "Leptin" $FILE
+echo
+echo
+grep "Serum albumin" $FILE
+echo
+echo
+grep "P62988" $FILE
+echo
+echo
+grep "Catalase" $FILE
+echo
+echo
+grep "Cytochrome b5" $FILE
+echo
+echo
+grep "Pro-epidermal" $FILE
+echo
+echo
+grep "Myoglobin" $FILE
+echo
+echo
+grep "NAD(P)H" $FILE
+echo
+echo
+grep "P62937" $FILE
+echo
+echo
+grep "Peroxiredoxin" $FILE
+echo
+echo
+grep "ubiquitin-related" $FILE
+echo
+echo
+grep "lactalalbumin" $FILE
+grep "P00709" $FILE
+echo
+echo
+grep "Creatine" $FILE
+echo
+echo
+grep "Lysozyme" $FILE
+echo
+echo
+grep "NEDD8" $FILE
+echo
+echo
+grep "Retinol" $FILE
+echo
+echo
+grep "Ribosyldihydronicotinamide" $FILE
+echo
+echo
+grep "SUMO-conjugating" $FILE
+echo
+echo
+
+
+grep "Antithrombin" $FILE
+echo
+echo
+grep "microglobulin" $FILE
+echo
+echo
+grep "death agonist" $FILE
+echo
+echo
+grep "synuclein" $FILE
+echo
+echo
+grep "Glutathione S-transferase A1" $FILE
+echo
+echo
+grep "Insulin-like growth factor" $FILE
+echo
+echo
+grep "Platelet-derived" $FILE
+echo
+echo
+grep "P10599" $FILE
+echo
+echo
+grep "P99999" $FILE
+echo
+echo
+grep "Gelsolin" $FILE
+echo
+echo
+grep "Glutathione S-transferase P" $FILE
+echo
+echo
+grep "GTPase Hras" $FILE
+echo
+echo
+grep "Interferon" $FILE
+echo
+echo
+grep "Serotransferrin" $FILE
+echo
+echo
+grep "Ubiquitin-conjugating enzyme E2 C" $FILE
+echo
+echo
+grep "Ubiquitin-conjugating enzyme E2 E1" $FILE
+echo
+echo
+grep "Annexin A5" $FILE
+echo
+echo
+grep "C-reactive" $FILE
+echo
+echo
+grep "Fatty acid binding protein" $FILE
+grep "Fatty acid-binding protein" $FILE
+grep "P05413" $FILE
+echo
+echo
+grep "Interleukin" $FILE
+grep "Interleukin-8" $FILE
+echo
+echo
+grep "Lactotransferrin" $FILE
+grep "Lactoferrin" $FILE
+echo
+echo
+grep "Microtubule-associated protein tau" $FILE
+grep "P10636" $FILE
+echo
+echo
+grep "Superoxide dismutase" $FILE
+echo
+echo
+grep "necrosis" $FILE
+echo
+echo
+
+
---
+++ proteins/untitled.sh
@@ -0,0 +1,48 @@
+echo
+echo
+grep "P63279" myrimatch_Extended_PSM_Report.txt
+echo
+echo
+
+
+
+grep "P62988" myrimatch_Default_Protein_Report.txt
+grep "P00167" myrimatch_Default_Protein_Report.txt
+grep "P62937" myrimatch_Default_Protein_Report.txt
+grep "P63165" myrimatch_Default_Protein_Report.txt
+grep "P00709" myrimatch_Default_Protein_Report.txt
+# grep "P12081" myrimatch_Default_Protein_Report.txt
+grep "P61626" myrimatch_Default_Protein_Report.txt
+grep "P06396" myrimatch_Default_Protein_Report.txt
+grep "P63279" myrimatch_Default_Protein_Report.txt
+grep "P02787" myrimatch_Default_Protein_Report.txt
+
+grep "O76070" myrimatch_Default_Protein_Report.txt
+grep "O76070" myrimatch_Default_Protein_Report.txt
+
+
+
+grep "P62988" myrimatch_Extended_PSM_Report.txt
+grep "P62937" myrimatch_Extended_PSM_Report.txt
+grep "P63165" myrimatch_Extended_PSM_Report.txt
+grep "P00709" myrimatch_Extended_PSM_Report.txt
+grep "P12081" myrimatch_Extended_PSM_Report.txt
+grep "P61626" myrimatch_Extended_PSM_Report.txt
+grep "P63279" myrimatch_Extended_PSM_Report.txt
+grep "O76070" myrimatch_Extended_PSM_Report.txt
+grep "P06396" myrimatch_Extended_PSM_Report.txt
+
+
+grep "P62937" myrimatch_Default_Protein_Report.txt
+grep "P62937" myrimatch_Extended_PSM_Report.txt
+
+
+grep "P02788" myrimatch_Default_Protein_Report.txt
+
+grep "P01112" myrimatch_Extended_PSM_Report.txt
+
+
+
+
+
+
