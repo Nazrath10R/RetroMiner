aa70e3afb1bf02e34d47c5a6109a509697a61756
Nazrath10R <nazrath.nawaz@yahoo.de>
2017-04-24 20:19:31 +0100

    new API to extract parameters for mzid files using R library

--- API/API_projects.sh
+++ API/API_projects.sh
@@ -60,3 +60,77 @@ while read p; do
 	# sed -i -e 's/"//g' links.sh 		# remove quotation marks
 	sed -i -e 's/^/wget /' links.sh 	# insert wget command
 
+	echo
+	echo "Project metadata download successful"
+	echo
+	echo
+	echo
+	echo "Number of files in $PXD"
+	wc -l links.sh
+	echo
+	echo
+	echo "file download links"
+	# display links
+	# cat links.sh
+	echo
+	echo
+
+	echo
+	echo
+	echo "Start downloading files:"
+	echo
+	echo
+	
+	# extract first 4 download links
+	sed -n -e '1,4p' links.sh > links2.sh
+	# sh links.sh
+	sh links2.sh
+	echo
+	echo
+	rm *.mgf
+	rm *.MGF
+	echo "All files downloaded"
+
+	#### extract parameters
+	echo
+	echo
+	echo "Starting R parameter parsing"
+	echo
+	echo
+	module load R/3.3.1_with_lib
+	echo
+	echo
+	Rscript mzid_parameters.R
+	echo
+	echo
+	echo "parameter parsed"
+
+	# add accession number
+	echo
+	echo
+	sed -ie 's/^"[0-9]"/'$PXD'/' parameters.txt
+	echo
+	echo
+	cat parameters.txt >> final_table.txt
+	# cat parameters.txt >> krisi_projects_table.txt
+	echo
+	echo
+
+	# head test.parameters.txt
+
+	# remove unnecessary files
+	rm $PXD
+	rm links.sh
+	rm links2.sh
+	rm links2.sh-e
+	rm *.mzid
+	rm *.gz
+	rm *.mgf
+	rm parameters.txt
+	rm parameters.txte
+
+# done <krisi_projects.txt
+done <Q9UN81.txt
+
+
+
