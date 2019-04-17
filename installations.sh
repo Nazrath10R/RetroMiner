5235c1e786e8844e5d5e324477494ff0a37c3f7c
Nazrath <nazrath.nawaz@yahoo.de>
2019-04-17 21:02:36 +0100

    proteome message

--- installations.sh
+++ installations.sh
@@ -16,3 +16,16 @@ mkdir sizes
 # wget https://www.uniprot.org/proteomes/UP000005640
 
 Rscript scripts/src/r_installations.R
+
+cd ..
+
+
+echo
+echo "Please download the REFERENCE PROTEOME"
+echo "of the species of interest into the proteome folder"
+echo "and change the path in parameters"
+echo "e.g. Human - https://www.uniprot.org/proteomes/UP000005640"
+echo 
+
+
+
